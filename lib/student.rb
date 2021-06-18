68b54e8209e6fd798b7ce58d69454f744a0a3612
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 15:42:01 -0400

    Archive Flatiron Code.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,11 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+  def initialize(attr_hash=nil)
+      attr_hash.each{|key, value| self.send("#{key.to_s}=", value) unless !self.respond_to?("#{key.to_s}=")} unless !attr_hash
+  end
 end
